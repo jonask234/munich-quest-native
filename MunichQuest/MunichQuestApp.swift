56a26a2bfc957a28184d08389d98fc4dbd70dd58
Jonas Klotsche <klotschejonas@gmail.com>
2025-11-12 12:17:48 +0800

    Initial Commit

---
+++ MunichQuest/MunichQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MunichQuestApp.swift
+//  MunichQuest
+//
+//  Created by Jonas Klotsche on 12.11.25.
+//
+
+import SwiftUI
+
+@main
+struct MunichQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
